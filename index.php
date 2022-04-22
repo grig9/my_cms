f9264993d1081e2dcb13b3b9460ba7144d20bae7
grig9 <70834200+grig9@users.noreply.github.com>
2022-04-22 16:11:02 +0300

    ывап

--- index.php
+++ index.php
@@ -1,17 +1,87 @@
 <?php
 
-class Test 
+
+
+class Date
 {
-  public function greeting($name)
+  public function __construct($date = null)
   {
-    echo "Hello, $name. How are you?<br>";
+    // если дата не передана - пусть берется текущая
+  }
+
+  public function getDay()
+  {
+    // возвращает день
+  }
+
+  public function getMonth($lang = null)
+  {
+    // возвращает месяц
+
+    // переменная $lang может принимать значение ru или en
+    // если эта не пуста - пусть месяц будет словом на заданном языке
+  }
+
+  public function getYear()
+  {
+    // возвращает год
+  }
+
+  public function getWeekDay($lang = null)
+  {
+    // возвращает день недели
+
+    // переменная $lang может принимать значение ru или en
+    // если эта не пуста - пусть день будет словом на заданном языке
+  }
+
+  public function addDay($value)
+  {
+    // добавляет значение $value к дню
+  }
+
+  public function subDay($value)
+  {
+    // отнимает значение $value от дня
+  }
+
+  public function addMonth($value)
+  {
+    // добавляет значение $value к месяцу
+  }
+
+  public function subMonth($value)
+  {
+    // отнимает значение $value от месяца
+  }
+
+  public function addYear($value)
+  {
+    // добавляет значение $value к году
+  }
+
+  public function subYear($value)
+  {
+    // отнимает значение $value от года
+  }
+
+  public function format($format)
+  {
+    // выведет дату в указанном формате
+    // формат пусть будет такой же, как в функции date
+  }
+
+  public function __toString()
+  {
+    // выведет дату в формате 'год-месяц-день'
   }
 }
 
-(new Test)->greeting('Sasha');
-(new Test)->greeting('Masha');
+
+
 
 exit;
+
 
 ini_set("display_errors", 1);
 error_reporting(E_ALL);
